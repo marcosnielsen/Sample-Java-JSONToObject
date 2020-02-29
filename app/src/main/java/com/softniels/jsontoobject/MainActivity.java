c3373f830ac42f1b1dfdeedecc7c10722b2041ca
marcos_nielsen <marcos@softniels.com.br>
2020-02-29 11:51:41 -0300

    settext to activty

--- app/src/main/java/com/softniels/jsontoobject/MainActivity.java
+++ app/src/main/java/com/softniels/jsontoobject/MainActivity.java
@@ -4,23 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private String jsonValue;
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.myLabel);
         WeatherEntity weatherEntity;
 
-        JSONToObject JSONToObject = new JSONToObject();
+        ParseJSONToWeatherEntity ParseJSONToWeatherEntity = new ParseJSONToWeatherEntity();
         this.jsonValue = "{\"by\":\"default\",\"valid_key\":false}";
-        weatherEntity = JSONToObject.JSONToObject(this.jsonValue);
+        weatherEntity = ParseJSONToWeatherEntity.JSONToObject(this.jsonValue);
         Log.i("weatherEntity", weatherEntity.getBy());
         if (weatherEntity.getValid_key()) {
+            textView.setText("Chave valida");
             Log.i("weatherEntity", "Chave valida");
         } else {
+            textView.setText("A Chave é inválida");
             Log.i("weatherEntity", "Chave inválida");
         }
     }
